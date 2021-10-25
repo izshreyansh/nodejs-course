6b475412df636bc35942e3989cd9f5498e248483
Shreyansh Panchal <sunpan835@gmail.com>
2021-10-25 20:45:33 +0530

    chalk

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 console.log(
-    getNotes()
+    validator.isURL('http://sh')
+)
+
+console.log(
+    chalk.bold.red('No Drivers are available.')
+)
+
+console.log(
+    chalk.bold.yellow('Taxi is available right now.')
 )
